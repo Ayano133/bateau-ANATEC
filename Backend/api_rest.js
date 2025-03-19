0c57fdf82247da2dc72def86886ee13eeb5659bb
Mattéo <pereiramatteo31@gmail.com>
2025-03-19 09:39:56 +0100

    Port 3001

--- Backend/api_rest.js
+++ Backend/api_rest.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const port = 3000;
+const port = 3001;
 const app = express();
 
-app. get("/post", (req, res) => {
-    res. json({ message: "Voici les données" });
+app.get("/post", (req, res) => {
+    res.json({ message: "Voici les données" });
 });
 
-app. listen(port, () => console. log(`Serveur a démarré au port` + port));
+app.listen(port, () => console.log(`Serveur a démarré au port` + port));
